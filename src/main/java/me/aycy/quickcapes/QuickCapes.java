72af5818a1506600685dee389b1fd82412f8c359
xItsSunny <luk.kliem@gmail.com>
2025-05-06 16:23:35 +0200

    quazy update
    (common cape)

--- src/main/java/me/aycy/quickcapes/QuickCapes.java
+++ src/main/java/me/aycy/quickcapes/QuickCapes.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(
         modid = "quickcapes",
         name = "Quick Capes",
-        version = "1.7",
+        version = "1.8",
         clientSideOnly = true,
         acceptedMinecraftVersions = "[1.8.9]"
 )
